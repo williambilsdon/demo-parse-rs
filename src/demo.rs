34c27a4e7c4541420558800f608e091e9fc99344
williambilsdon <williambilsdon@gmail.com>
2023-05-13 20:53:03 +0100

    feat : parse entire header file

--- src/demo.rs
+++ src/demo.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use bitreader_rs::Bitreader;
 
+#[derive(PartialEq, Debug)]
 struct Demo {
     header: String,
     demo_protocol: i32,
@@ -17,14 +18,34 @@ struct Demo {
 
 // TODO: All unwraps need proper handling.
 impl Demo {
-    pub fn parse(file_name: &str) {
+    pub fn parse(file_name: &str) -> Demo {
         let file = fs::read(file_name).unwrap();
-        
         let mut bitreader = Bitreader::new(file.as_slice());
-        
-        let demo_header = bitreader.read_string(8).unwrap();
-        println!("{}", demo_header)
 
+        let header = bitreader.read_string(8).unwrap();
+        let demo_protocol = bitreader.read_i32().unwrap();
+        let network_protocol = bitreader.read_i32().unwrap();
+        let server_name = bitreader.read_string(260).unwrap();
+        let client_name = bitreader.read_string(260).unwrap();
+        let map_name = bitreader.read_string(260).unwrap();
+        let game_directory = bitreader.read_string(260).unwrap();
+        let playback_time = bitreader.read_f32().unwrap();
+        let ticks = bitreader.read_i32().unwrap();let frames = bitreader.read_i32().unwrap();
+        let sign_on_length = bitreader.read_i32().unwrap();  
+
+        Demo { 
+            header, 
+            demo_protocol, 
+            network_protocol, 
+            server_name, 
+            client_name, 
+            map_name, 
+            game_directory, 
+            playback_time,
+            ticks, 
+            frames, 
+            sign_on_length
+        }
     }
 }
 
@@ -34,7 +55,20 @@ mod tests {
 
     #[test]
     fn parse() {
+        let expected = Demo { 
+            header: String::from("HL2DEMO"), 
+            demo_protocol: 67108864, 
+            network_protocol: 557187072, 
+            server_name: String::from("localhost:27015"), 
+            client_name: String::from("will"), 
+            map_name: String::from("de_dust2"), 
+            game_directory: String::from("csgo"), 
+            playback_time: 0.000000000000000000000000000000000000019110306,
+            ticks: 116260863, 
+            frames: -535232512, 
+            sign_on_length: -1685975552
+        };
         let result = Demo::parse("test_demo.dem");
-        assert_eq!(result, ())
+        assert_eq!(result, expected)
     }
 }
