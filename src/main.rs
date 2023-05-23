663e1585058c9677b250a1200de2eb7817a935d0
William Bilsdon <williambilsdon@icloud.com>
2023-05-23 07:19:06 +0100

    wip : adding cli args for file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod demo;
 
-use std::{fs, process};
-
+use std::{fs, process, env};
 use crate::demo::Demo;
 
 fn main() {
-    let file_name = "test_demo.dem";
+    let args: Vec<String> = env::args().collect();
+    if args.is_empty() {
+        eprintln!("Error: No provided arguments.");
+        process::exit(1)
+    }
+    let file_name = &args[0];
     let file = fs::read(file_name).unwrap_or_else(|err| {
         eprintln!("Error reading file: {}", err);
         process::exit(1)
